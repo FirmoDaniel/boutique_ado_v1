2dc1f7005905498f49a5cfc8012df7e821e0a4e5
FirmoDaniel <firmodaniel@gmail.com>
2021-06-09 13:55:42 +0000

    adding products 3:13 in.

---
+++ products/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
