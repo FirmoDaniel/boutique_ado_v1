c6d2e73bf8265c239375c8d1c61f0c1bef98a4d5
FirmoDaniel <firmodaniel@gmail.com>
2021-06-10 11:19:22 +0000

    products page working ish.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to return all products , including sorting and searching """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
