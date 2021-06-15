11b2754165961b16788d794f62fe6f6f867be95f
FirmoDaniel <firmodaniel@gmail.com>
2021-06-15 12:37:29 +0000

    add to cart working.

--- bag/views.py
+++ bag/views.py
@@ -8,6 +8,7 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -21,5 +22,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
