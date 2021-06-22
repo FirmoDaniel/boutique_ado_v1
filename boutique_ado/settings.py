f9139d636e5040a7001f725ccb9f55c85850c994
FirmoDaniel <firmodaniel@gmail.com>
2021-06-22 13:28:09 +0000

    quicksave checkout now working.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -79,9 +79,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-            'builtins' :[
+            'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
             ]
